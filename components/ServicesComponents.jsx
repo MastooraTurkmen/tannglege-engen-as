f23b0c24686186c4e0d338903c3a66b2bb71c15f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-28 21:15:48 +0430

    services component

---
+++ components/ServicesComponents.jsx
@@ -0,0 +1,27 @@
+import { services } from "@/contract/services";
+
+const ServicesComponents = () => {
+  return (
+    <div className="flex flex-row flex-wrap my-10 text-center justify-center">
+      {services.map((service) => {
+        const { id, title, description, image } = service;
+        return (
+          <div
+            key={id}
+            className="flex flex-col w-[300px] lg:w-[400px] shadow-xl lg:p-10 p-5 m-5 border rounded-xl justify-center items-center"
+          >
+            <img
+              className="lg:w-[320px] w-full h-[200px] lg:h-[260px] object-cover rounded-xl"
+              src={image}
+              alt={title}
+            />
+            <h2 className="lg:text-2xl text-xl my-5 font-semibold">{title}</h2>
+            <p className="text-sm lg:text-base">{description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ServicesComponents;
