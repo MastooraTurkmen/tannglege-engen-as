27a4775bf2f2446cdef5100da4cd82eda0daa216
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-28 22:15:53 +0430

    section 3 completed

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero"
 import Navbar from "@/components/Navbar"
 import Section2 from "@/components/Section2"
+import Sections3 from "@/components/Section3"
 import Sections from "@/components/Sections"
 import Services from "@/components/Services"
 
@@ -12,6 +13,7 @@ const Home = () => {
       <Services />
       <Sections />
       <Section2 />
+      <Sections3 />
     </div>
   )
 }
