08c62a0fcd38753361c0de7e6fe563a5a0bc7231
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-28 21:16:25 +0430

    adds data

---
+++ contract/services.js
@@ -0,0 +1,20 @@
+export const services = [
+    {
+        id: 1,
+        title: 'Dental Care',
+        description: "Our experienced team of dentists provides a wide range of preventive, restorative, and cosmetic services to keep your smile healthy and beautiful.",
+        image: '/images/white-tooth.png'
+    },
+    {
+        id: 2,
+        title: 'Cosmetic Dentistry',
+        description: "Our cosmetic dentistry treatments, such as teeth whitening, can enhance the aesthetic appearance of your smile and boost your confidence.",
+        image: '/images/Cosmetic.jpeg'
+    },
+    {
+        id: 3,
+        title: 'Orthodontic Services',
+        description: "We provide a variety of orthodontic treatment options, including traditional braces and clear aligners, to straighten your teeth.",
+        image: '/images/ortho-braces-spotlight.png'
+    },
+]
