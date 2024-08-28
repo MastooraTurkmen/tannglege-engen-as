d96495d621b5499071e52f16148df747f29a3194
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-28 21:16:14 +0430

    adds link data

---
+++ contract/link.js
@@ -0,0 +1,22 @@
+export const link = [
+    {
+        id: 1,
+        text: 'Home',
+        path: '/'
+    },
+    {
+        id: 2,
+        text: 'About us',
+        path: '/about'
+    },
+    {
+        id: 3,
+        text: 'Prices',
+        path: '/prices'
+    },
+    {
+        id: 4,
+        text: 'Services',
+        path: '/services'
+    }
+]
