cdfd08b01594ad800ac5f73f1b8661610f1dc4dd
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-28 21:14:54 +0430

    set up

--- app/page.js
+++ app/page.js
@@ -1,6 +1,18 @@
+import Hero from "@/components/Hero"
+import Navbar from "@/components/Navbar"
+import Section2 from "@/components/Section2"
+import Sections from "@/components/Sections"
+import Services from "@/components/Services"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Sections />
+      <Section2 />
+    </div>
   )
 }
 
