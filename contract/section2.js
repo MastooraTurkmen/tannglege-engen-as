65df59dc66a724a25d57c222e8fa50b806c6dc9d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-28 21:16:21 +0430

    adds data

---
+++ contract/section2.js
@@ -0,0 +1,20 @@
+export const services2 = [
+    {
+        id: 1,
+        title: '10.324',
+        description: "Successfully installed implants",
+        image: '/images/Card-image-1.png'
+    },
+    {
+        id: 2,
+        title: '4.8',
+        description: "Average Rating",
+        image: '/images/Cosmetic.jpeg'
+    },
+    {
+        id: 3,
+        title: 'Average',
+        description: "Procedure",
+        image: '/images/ortho-braces-spotlight.png'
+    },
+]
