82826d2f738da304cc74d1f36fe1c70d1afa7210
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-29 20:29:45 +0430

    added testimonial section

---
+++ contract/testimonial.js
@@ -0,0 +1,37 @@
+export const testimonial = [
+    {
+        id: 0,
+        person: 'Rafid Hoda',
+        description: "Beste tannlegen jeg noensinne har besøkt :) Utrolig professionelt, hyggelig og rimelig!",
+        image: '/images/Rafid-Hoda.png',
+        rating: '5.0'
+    },
+    {
+        id: 1,
+        person: 'Raven Shalash',
+        description: "Jeg er veldig fornøyd med tjenesten jeg fikk. Smertefri behandling, hyggelig og fine damer, 100% anbefalt",
+        image: '/images/Raven.png',
+        rating: '5.0'
+    },
+    {
+        id: 2,
+        person: 'Chikorita2Chante',
+        description: "Been going here for years. Through bad tooth genes, yearly check ups and teeth cleaning sessions.",
+        image: '/images/Chikorita2Chante.png',
+        rating: '5.0'
+    },
+    {
+        id: 3,
+        person: 'Majgrit',
+        description: "Super happy with the service! In addition to fixing a tooth, these ladies actually made me overcome a horrible fear of dentists.",
+        image: '/images/Majgrit.png',
+        rating: '5.0'
+    },
+    {
+        id: 4,
+        person: 'chenrik kirnehc',
+        description: "Great service and professional job.",
+        image: '/images/Chenrik.png',
+        rating: '5.0'
+    },
+]
