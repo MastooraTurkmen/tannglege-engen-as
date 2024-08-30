49c0dac95b6e5274c4de7e931385999ebd0a2810
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-30 21:17:11 +0430

    clean code

---
+++ components/index.js
@@ -0,0 +1,11 @@
+export { default as Button } from './Button';
+export { default as Hero } from './Hero';
+export { default as Navbar } from './Navbar';
+export { default as Testimonial } from './Testimonial';
+export { default as TestimonialComponent } from './TestimonialComponent';
+export { default as Section2 } from './Section2';
+export { default as Section3 } from './Section3';
+export { default as Services } from './Services';
+export { default as Sections } from './Sections';
+export { default as ServicesComponents } from './ServicesComponents';
+export { default as Footer } from './Footer';
