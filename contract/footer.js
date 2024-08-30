e0fe706692414fda504069b93527a5feb562e810
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-30 21:18:06 +0430

    footer

---
+++ contract/footer.js
@@ -0,0 +1,17 @@
+export const footerLinks = [
+    {
+        id: 1,
+        title: 'E-post',
+        description: "post@tannengen.no",
+    },
+    {
+        id: 2,
+        title: 'Adresse',
+        description: "Tannlege Ann-Kristin Engen ASWaldemar Thranes gate 50171 Oslo",
+    },
+    {
+        id: 3,
+        title: 'Telefon',
+        description: "+47 22 46 10 24",
+    },
+]
