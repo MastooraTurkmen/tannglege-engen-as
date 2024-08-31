db6d9f846aacdb6dcfbf5b7a949eb96504f383a9
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-31 22:11:41 +0430

    location page

---
+++ components/Location.jsx
@@ -0,0 +1,16 @@
+'use client';
+
+const Location = () => {
+  return (
+    <div className="flex m-14 flex-col justify-center text-center items-center text-black">
+      <h1 className="md:text-3xl z-10 text-xl font-bold">Where we located</h1>
+      <div className="bg-[#00B8FF] z-0 mt-[-9px] py-1 md:px-[130px] px-[90px] border-none"></div>
+      <p className="md:text-base text-sm lg:text-xl m-5 lg:w-[500px]">
+        We are located on the second floor, the entrance to the right of Coop
+        Prix.
+      </p>
+    </div>
+  );
+};
+
+export default Location;
