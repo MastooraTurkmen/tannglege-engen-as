9b93238c56f138570a5d7ac4ac38333c7667039d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-31 22:10:30 +0430

    prices

---
+++ contract/prices.js
@@ -0,0 +1,47 @@
+export const pricesList = [
+    {
+        id: 1,
+        type: 'Undersøkelse, 2 røntgen og grundig rens',
+        price: "1495",
+    },
+    {
+        id: 2,
+        type: 'Tannfarget fylling, 1 flate',
+        price: "1060",
+    },
+    {
+        id: 3,
+        type: 'Tannfarget fylling, 2 flate',
+        price: "1710",
+    },
+    {
+        id: 4,
+        type: 'Tannfarget fylling, 3 flater',
+        price: "2530",
+    },
+    {
+        id: 5,
+        type: 'Krone av porselen/gull - alt inkludert',
+        price: "7500",
+    },
+    {
+        id: 6,
+        type: 'Rotfylling, 1 kanal',
+        price: "4445",
+    },
+    {
+        id: 7,
+        type: 'Rotfylling, 2 kanaler',
+        price: "5880",
+    },
+    {
+        id: 8,
+        type: 'Rotfylling, 2 kanaler',
+        price: "7000",
+    },
+    {
+        id: 9,
+        type: 'Kirurgisk fjerning av visdomstann',
+        price: "4200",
+    },
+]
