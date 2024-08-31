38a10fb6c15ef62bfcce2b1e53e4eb5dc9639453
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-31 22:12:49 +0430

    section five component

---
+++ components/Section5.jsx
@@ -0,0 +1,24 @@
+import { Button } from ".";
+
+const Section5 = ({ children }) => {
+  return (
+    <div className="flex text-white mb-10 bg-[hsl(197,100%,18%)] flex-col sm:flex-row">
+      <img
+        style={{
+          flexBasis: "40%",
+        }}
+        className="max-h-[550px] sm:h-[500px] object-cover"
+        src={children || "/images/Ann-Kristin.jpg"}
+        alt="dintents"
+      />
+      <div className="py-24 m-auto xl:mx-40 mx-10">
+        <h1 className="text-xl flex mb-10 font-bold md:text-3xl lg:text-4xl">
+          Book an appointment digitally with your dentist at Oslo
+        </h1>
+        <Button link="#footer" text="Book an Appointment" />
+      </div>
+    </div>
+  );
+};
+
+export default Section5;
