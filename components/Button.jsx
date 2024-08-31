f6beec5e218d24efc6530dba71e1417be83d756a
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-31 22:11:09 +0430

    changes button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-const Button = ({ change, text }) => {
+const Button = ({ change, text, link }) => {
   return (
     <Link
-      className={` ${change} bg-[#00B8FF] btn text-[#00415A] hover:text-white 
+      className={` ${change} shadow-lg bg-[#00B8FF] btn text-[#00415A] hover:text-white 
       rounded-3xl border-none px-7 text-[13px] items-center md:text-[16px] lg:text-xl`}
-      href="#footer"
+      href={link}
     >
       {text}
     </Link>
