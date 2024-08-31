8c4ea86b05618864e723abbb3806590dba59608a
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-31 22:10:22 +0430

    data js

---
+++ contract/data.js
@@ -0,0 +1,53 @@
+export const data = [
+    {
+        id: 1,
+        text: 'With us, expertise and care combine for an experience that makes you smile.',
+    },
+    {
+        id: 2,
+        text: 'With us, expertise and care combine for an experience that makes you smile.',
+    },
+    {
+        id: 3,
+        text: 'With us, expertise and care combine for an experience that makes you smile.',
+    },
+    {
+        id: 4,
+        text: 'With us, expertise and care combine for an experience that makes you smile.',
+    },
+]
+
+export const open = [
+    {
+        id: 1,
+        day: 'Monday to Thursday',
+        time: "07:00 – 15:00",
+    },
+    {
+        id: 2,
+        day: 'Friday',
+        time: "07:00 – 13:00",
+    },
+    {
+        id: 3,
+        day: 'Saturday, Sunday and public holidays',
+        time: "Closed",
+    },
+]
+
+
+export const imagse = [
+    {
+        id: 1,
+        image: '/images/Katrin.png',
+    },
+    {
+        id: 2,
+        image: '/images/Ann.png',
+    },
+    {
+        id: 3,
+        image: '/images/Ellen.png',
+    },
+]
+
