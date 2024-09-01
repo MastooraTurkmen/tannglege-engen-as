743c9487e1dfd138771b93c7443f57add776657b
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-01 21:01:28 +0430

    convert to norwegian

---
+++ app/Tjenester/page.jsx
@@ -0,0 +1,13 @@
+import { OpeningHourse, Section5, Services } from "@/components";
+
+const page = () => {
+  return (
+    <div className="pt-24">
+      <Services />
+      <OpeningHourse />
+      <Section5 />
+    </div>
+  );
+};
+
+export default page;
