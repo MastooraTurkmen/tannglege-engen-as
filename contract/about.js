07dd47ffc795efb3765d1b0337cae442083858ca
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-01 19:48:16 +0430

    about js

---
+++ contract/about.js
@@ -0,0 +1,29 @@
+export const about = [
+    {
+        id: 1,
+        image: '/images/Ann-Kristin-image.jpg',
+        name: 'Ann-Kristin Engen',
+        position: "Tannlege MNTF",
+        about1: 'Praksisinnehaver siden 2005',
+        about2: 'Uteksaminert fra Det odontologiske fakultet i Oslo 1992',
+        about3: 'Erfaring fra offentlig og privat tannhelsetjeneste i Ås og Oslo.',
+    },
+    {
+        id: 2,
+        image: '/images/Ellen.jpg',
+        name: 'Ellen Hodt Smith',
+        position: "Tannpleier MNTP",
+        about1: 'Ellen har jobbet sammen med oss siden 1998.',
+        about2: 'Uteksaminert fra Det odontologiske fakultet i Oslo 1988',
+        about3: 'Bred erfaring fra privat tannhelsetjeneste.',
+    },
+    {
+        id: 3,
+        image: '/images/Katrin.jpg',
+        name: 'Catrine Ødemark',
+        position: "Tannlegesekretær",
+        about1: 'Catrine har jobbet sammen med oss fra 1997.',
+        about2: 'Uteksaminert fra Sandaker v. skole 1989. Utdannet teknisk tegner fra Manglerud v. skole 1991.',
+        about3: 'Bred erfaring innen salg, service og kundebehandling.',
+    },
+]
