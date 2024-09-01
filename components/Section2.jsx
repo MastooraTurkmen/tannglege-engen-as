462ce3056fd23b875de80bed423e76a4fa8a41a8
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-01 20:37:59 +0430

    convert to norwegian

--- components/Section2.jsx
+++ components/Section2.jsx
@@ -3,7 +3,9 @@ import { services } from "@/contract/services";
 const Section2 = () => {
   return (
     <div className="flex m-14 flex-col justify-center text-center items-center text-black">
-      <h1 className="md:text-3xl text-xl font-bold">Våre tannhelsetjenester</h1>
+      <h1 className="md:text-3xl text-xl font-bold">
+        Våre tannlegeprestasjoner
+      </h1>
       <div className="bg-[#00B8FF] mt-[-8px] py-1 md:px-[140px] px-[90px] border-none"></div>
       {/* Card Compontent */}
       <div className="flex flex-row flex-wrap m-10 text-center justify-center">
