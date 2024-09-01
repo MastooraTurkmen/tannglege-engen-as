f6cfa1ae5d45b00562ac7bc5764a2c10d00e475f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-01 20:37:57 +0430

    convert to norwegian

--- app/loading.jsx
+++ app/loading.jsx
@@ -6,10 +6,6 @@ const override = {
   margin: "100px auto",
 };
 
-// NOTE: Here 'loading' will be undefined as a loading file doesn't receive any
-// props:
-// https://nextjs.org/docs/app/api-reference/file-conventions/loading
-
 const LoadingPage = () => {
   return (
     <div className="h-[100vh] pt-56">
