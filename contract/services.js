16d47222326d98c48085fe8b483f40779a1b8ad0
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-01 20:17:25 +0430

    convert to norwegian

--- contract/services.js
+++ contract/services.js
@@ -2,19 +2,19 @@ export const services = [
     {
         id: 1,
         title: 'Dental Care',
-        description: "Our experienced team of dentists provides a wide range of preventive, restorative, and cosmetic services to keep your smile healthy and beautiful.",
+        description: "Vårt erfarne team av tannleger tilbyr et bredt spekter av forebyggende, gjenopprettende og kosmetiske tjenester for å holde smilet ditt sunt og vakkert.",
         image: '/images/white-tooth.png'
     },
     {
         id: 2,
         title: 'Cosmetic Dentistry',
-        description: "Our cosmetic dentistry treatments, such as teeth whitening, can enhance the aesthetic appearance of your smile and boost your confidence.",
+        description: "Våre kosmetiske tannbehandlinger, som tannbleking, kan forbedre det estetiske utseendet til smilet ditt og øke selvtilliten din.",
         image: '/images/Cosmetic.jpeg'
     },
     {
         id: 3,
         title: 'Orthodontic Services',
-        description: "We provide a variety of orthodontic treatment options, including traditional braces and clear aligners, to straighten your teeth.",
+        description: "Vi tilbyr en rekke kjeveortopedisk behandlingsalternativer, inkludert tradisjonelle tannreguleringer og klare aligners, for å rette tennene dine.",
         image: '/images/ortho-braces-spotlight.png'
     },
 ]
