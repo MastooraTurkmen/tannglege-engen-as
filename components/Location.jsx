a0d8623337d125aa4f344fbc5ae5e9c8b5ebdaf4
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-01 20:16:47 +0430

    convert to norwegian

--- components/Location.jsx
+++ components/Location.jsx
@@ -1,13 +1,14 @@
-'use client';
+"use client";
 
 const Location = () => {
   return (
     <div className="flex m-14 flex-col justify-center text-center items-center text-black">
-      <h1 className="md:text-3xl z-10 text-xl font-bold">Where we located</h1>
+      <h1 className="md:text-3xl z-10 text-xl font-bold">
+        Hvor vi lokaliserte
+      </h1>
       <div className="bg-[#00B8FF] z-0 mt-[-9px] py-1 md:px-[130px] px-[90px] border-none"></div>
       <p className="md:text-base text-sm lg:text-xl m-5 lg:w-[500px]">
-        We are located on the second floor, the entrance to the right of Coop
-        Prix.
+        Vi holder til i andre etasje, inngangen til høyre for Coop Prix.
       </p>
     </div>
   );
