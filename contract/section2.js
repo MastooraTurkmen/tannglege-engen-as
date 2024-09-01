4fe003f87beb1f5a6c51a84bfa4e93bdde094841
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-01 20:17:21 +0430

    convert to norwegian

--- contract/section2.js
+++ contract/section2.js
@@ -2,19 +2,19 @@ export const services2 = [
     {
         id: 1,
         title: '10.324',
-        description: "Successfully installed implants",
+        description: "Vellykket installerte implantater",
         image: '/images/Card-image-1.png'
     },
     {
         id: 2,
         title: '4.8',
-        description: "Average Rating",
+        description: "Gjennomsnittlig vurdering",
         image: '/images/Cosmetic.jpeg'
     },
     {
         id: 3,
         title: 'Average',
-        description: "Procedure",
+        description: "Prosedyre",
         image: '/images/ortho-braces-spotlight.png'
     },
 ]
