a5f0589da02ae67e4348ec78562e409602eb28dd
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-01 20:17:14 +0430

    convert to norwegian

--- components/Services.jsx
+++ components/Services.jsx
@@ -3,7 +3,7 @@ import ServicesComponents from "./ServicesComponents";
 const Services = () => {
   return (
     <div className="flex m-14 flex-col justify-center text-center items-center text-black">
-      <h1 className="md:text-3xl text-xl font-bold">Our Dental Services</h1>
+      <h1 className="md:text-3xl text-xl font-bold">Våre tannhelsetjenester</h1>
       <div className="bg-[#00B8FF] mt-[-8px] py-1 md:px-[140px] px-[90px] border-none"></div>
       <ServicesComponents />
     </div>
