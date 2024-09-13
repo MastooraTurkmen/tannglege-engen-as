9b650960750797666b6408528621ef35dd7d619c
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-13 19:59:04 +0430

    adds new items

--- components/AboutComponent2.jsx
+++ components/AboutComponent2.jsx
@@ -3,8 +3,8 @@ import { about } from "@/contract/about";
 const AboutComponent2 = () => {
   return (
     <div className="flex m-14 mb-0 lg:mx-28 xl:mx-36 2xl:mx-52 flex-col justify-center text-center items-center text-black">
-      <h1 className="md:text-3xl z-10 text-xl font-bold">Hvem er vi s</h1>
-      <div className="bg-[hsl(197,100%,50%)] mb-10 z-0 -mt-2 py-1.5 md:px-24 px-12 border-none"></div>
+      <h1 className="md:text-3xl z-10 text-2xl font-bold">Hvem er vi s</h1>
+      <div className="bg-[hsl(197,100%,50%)] mb-10 z-0 -mt-2 py-1.5 md:px-24 px-16 border-none"></div>
       {about.map((item) => {
         const { id, name, position, about1, about2, about3, image } = item;
         const order = item.id - 2;
@@ -20,17 +20,17 @@ const AboutComponent2 = () => {
               className={` py-10 sm:py-0 lg:p-14 p-5 lg:py-10 flex flex-col justify-between m-auto text-center items-center
                 ${order ? "order-1" : "order-2"}`}
             >
-              <h1 className="lg:text-2xl z-10 md:text-2xl text-base font-bold">
+              <h1 className="lg:text-2xl z-10 md:text-2xl text-xl font-bold">
                 {name}
               </h1>
               <div className="bg-[#00B8FF] z-0 -mt-2 py-1.5 md:px-28 px-20 border-none"></div>
               <p
-                className="md:m-10 m-5 lg:text-2xl"
+                className="md:m-10 m-5 lg:text-2xl text-xl"
                 style={{ fontFamily: "Quintessential" }}
               >
                 {position}
               </p>
-              <div className="lg:text-2xl text-center p-2 text-[10px] sm:text-sm md:text-base">
+              <div className="lg:text-2xl text-center p-2 text-base">
                 <p>{about1}</p>
                 <p className="md:my-10 my-3">{about2}</p>
                 <p>{about3}</p>
@@ -41,7 +41,7 @@ const AboutComponent2 = () => {
                 flexBasis: "50%",
               }}
               src={image}
-              className={`rounded-xl sm:rounded-none object-center sm:object-top object-cover w-full max-h-[300px] min-w-[100px] sm:max-h-[700px] ${
+              className={`rounded-xl sm:rounded-none object-center sm:object-top object-cover w-full max-h-[450px] min-w-[100px] sm:max-h-[700px] ${
                 order ? "sm:order-2" : "order-1"
               }`}
               alt={name}
