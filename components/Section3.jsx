b4a92919042228ec8e3a033a8a1e9ebd95ca5f97
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-14 16:23:00 +0430

    new feature added

--- components/Section3.jsx
+++ components/Section3.jsx
@@ -1,11 +1,13 @@
+"use client";
+
 const Section3 = () => {
   return (
-    <div className="flex text-white bg-[hsl(197,100%,18%)] flex-col sm:flex-row">
-      <div className="flex py-24 xl:mx-40 flex-col justify-center mx-10">
-        <h1 className="text-xl mb-5 font-bold md:text-3xl lg:text-4xl">
+    <div className="flex text-white bg-[hsl(197,100%,18%)] overflow-hidden flex-col md:flex-row">
+      <div className="flex py-24 xl:my-20 xl:mx-20 flex-col justify-center mx-10">
+        <h1 className="text-2xl mb-5 font-bold md:text-3xl lg:text-4xl">
           Tannløsninger for smilet ditt
         </h1>
-        <p className="text-sm md:text-base lg:text-xl">
+        <p className="lg:text-xl">
           Vår omfattende tannlegepraksis tilbyr et bredt spekter av
           forebyggende, gjenopprettende og kosmetiske behandlinger for å dekke
           alle dine orale helsebehov. Fra rutinemessige rengjøringer og
@@ -15,7 +17,7 @@ const Section3 = () => {
         </p>
       </div>
       <img
-        className="sm:h-[500px] h-[400px] object-top object-cover"
+        className="object-top object-cover min-w-[550px] 2xl:min-w-[760px]"
         src="/new-images/women.png"
         alt="dintents"
       />
