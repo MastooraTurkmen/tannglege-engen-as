ea835093267e1cbcb819812cb04d4992f31b51d5
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-14 18:36:19 +0430

    makes some changes

--- components/Location.jsx
+++ components/Location.jsx
@@ -3,11 +3,11 @@
 const Location = () => {
   return (
     <div className="flex m-14 flex-col justify-center text-center items-center text-black">
-      <h1 className="md:text-3xl z-10 text-xl font-bold">
+      <h1 className="md:text-3xl lg:text-5xl z-10 text-2xl font-bold">
         Hvor vi lokaliserte
       </h1>
-      <div className="bg-[#00B8FF] z-0 mt-[-9px] py-1 md:px-[130px] px-[90px] border-none"></div>
-      <p className="md:text-base text-sm lg:text-xl m-5 lg:w-[500px]">
+      <div className="bg-[#00B8FF] z-0 mt-[-9px] py-1 md:px-[130px] lg:px-[200px] px-[90px] border-none"></div>
+      <p className="lg:text-xl m-5 lg:w-[500px]">
         Vi holder til i andre etasje, inngangen til høyre for Coop Prix.
       </p>
     </div>
