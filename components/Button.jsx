fcd9bfbd8d9e4f24cc13460474e7046b535d66db
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-15 17:58:00 +0430

    changed some features

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Button = ({ change, text, link }) => {
   return (
     <Link
-      className={` ${change} shadow-lg bg-[#00B8FF] btn text-[rgb(0,65,90)] scale-100 
+      className={` ${change} shadow-lg bg-[#00B8FF] hover:text-white btn text-[rgb(0,65,90)] scale-100 
       rounded-3xl border-none px-7 items-center lg:text-xl`}
       href={link}
     >
