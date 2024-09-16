89e3d5a482e426d7b7131dff965d9b2fc183068b
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-16 21:38:54 +0430

    fixed some lovely bugs

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 const Button = ({ change, text, link }) => {
   return (
     <Link
-      className={` ${change} shadow-lg bg-[#00B8FF] hover:text-white btn text-[rgb(0,65,90)] scale-100 
-      rounded-3xl border-none px-7 items-center lg:text-xl`}
+      className={` ${change} text-center shadow-lg bg-[#00B8FF] hover:text-white btn text-[rgb(0,65,90)] scale-100 
+      rounded-3xl text-base border-none px-7 items-center lg:text-xl`}
       href={link}
     >
       {text}
