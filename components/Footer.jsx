96734d4cf7f7519c0d396c9a4a3df6c438f9583a
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-16 21:39:06 +0430

    changed the features

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,37 +10,39 @@ const Footer = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ ease: "easeOut", duration: 2 }}
-      className="text-white footer-center bg-[#00415A] "
+      className="text-white bg-[#00415A] "
     >
-      <div className="flex overflow-hidden md:gap-2 lg:gap-0 flex-col md:flex-row justify-between text-center items-center py-24 p-10 lg:py-20">
+      <div className="flex flex-col 2xl:gap-20 overflow-hidden justify-between 2xl:justify-around items-center p-20 py-28 md:flex-row md:space-x-20 text-center md:text-left">
         <a href="/">
           <img
             src="/images/logo-no-background.png"
             alt="logo"
-            className="max-w-80"
+            className="max-w-80 mb-10 md:mb-0"
           />
         </a>
-        {footerLinks.map(({ id, title, description }, index) => {
-          const isLast = index === footerLinks.length - 2;
-          return (
-            <div key={id}>
+
+        <div className="flex flex-wrap justify-center 2xl:gap-20 3xl:gap-32 gap-10">
+          {footerLinks.map(({ id, title, description }) => {
+            return (
               <div
-                className={`lg:w-[300px] 2xl:w-[400px] py-24 flex h-40 md:h-24 md:w-[200px] flex-col ${
-                  isLast ? "md:border-x-2 md:border-y-0 border-y-2" : ""
-                } 
-              justify-center border-[rgb(0,184,255)] text-center items-center lg:text-base`}
+                key={id}
+                className="flex w-52 flex-col items-center md:items-start text-center md:text-left"
               >
-                <h1 className="mb-4">{title}</h1>
-                <p className="lg:w-48 md:m-auto">{description}</p>
+                <h1 className="mb-4 border-b-4 border-[rgb(0,184,255)] text-xl">
+                  {title}
+                </h1>
+                <p>{description}</p>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-      <p className="mb-14 px-5">
+
+      <p className="mb-14 text-center px-5">
         Copyright ©{new Date().getFullYear()} Dentist Ann-Kristin Engen AS
       </p>
-      <div className="bg-black text-white p-10">
+
+      <div className="bg-black text-white p-10 text-center">
         <h1>
           Design og utviklet av{" "}
           <a
