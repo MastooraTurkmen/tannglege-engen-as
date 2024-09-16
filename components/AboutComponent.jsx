7b2fee8b9b8959a16e522281b430d0cf5ec184a6
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-16 21:38:44 +0430

    fixed some lovely bugs

--- components/AboutComponent.jsx
+++ components/AboutComponent.jsx
@@ -2,7 +2,8 @@ import { about } from "@/contract/about";
 
 const AboutComponent = () => {
   return (
-    <div className="text-black">
+    <div className="text-black p-2">
+      {/* Header Section */}
       <div className="flex flex-col text-center justify-center items-center">
         <h1 className="md:text-3xl xl:text-5xl text-2xl z-10 font-bold">
           Møt nabolagets tannleger
@@ -13,25 +14,28 @@ const AboutComponent = () => {
           tannlegebehov.
         </p>
       </div>
-      <div className="flex mx-5 h-[900px] flex-wrap gap-10 text-center justify-center items-center mt-10">
-        {about.map(({ id, image, name, position, about1, about2, about3 }) => {
-          return (
-            <div key={id}>
-              <img
-                src={image}
-                alt={name}
-                className="h-[500px] w-[400px] object-cover mb-20 flex z-10 rounded-t-md "
-              />
-              <div className="p-10 w-[400px] flex flex-col mt-[-100px] justify-center shadow-md border rounded-md z-20 border-[#00415A] text-center">
-                <h1 className="text-2xl font-semibold">{name}</h1>
-                <em>{position}</em>
-                <p className="mt-5">{about1}</p>
-                <p className="my-5">{about2}</p>
-                <p>{about3}</p>
-              </div>
+
+      {/* Cards Section */}
+      <div className="flex flex-wrap gap-10 mx-5 justify-center mt-10">
+        {about.map(({ id, image, name, position, about1, about2, about3 }) => (
+          <div
+            key={id}
+            className="flex flex-col items-center text-center max-w-[400px] rounded-lg shadow-lg"
+          >
+            <img
+              src={image}
+              alt={name}
+              className="max-h-[500px] w-full object-cover rounded-t-md"
+            />
+            <div className="p-5 bg-white -mt-2 flex flex-col justify-center shadow-t-lg rounded-t-md">
+              <h1 className="text-2xl font-semibold">{name}</h1>
+              <em className="py-5">{position}</em>
+              <p className="mt-3">{about1}</p>
+              <p className="my-3">{about2}</p>
+              <p>{about3}</p>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
