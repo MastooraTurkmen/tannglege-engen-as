e3048111470fc7dd9477bec2fe31f1e171027bc0
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-09-01 20:17:53 +0430

    style page

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth overflow-x-hidden">
       <body className={inter.className}>
         <Navbar />
         <main>
